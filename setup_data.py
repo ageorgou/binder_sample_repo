484d787879d3b0722df85a3aa93b2f99acac9e50
Anastasis Georgoulas <a.georgoulas@ucl.ac.uk>
2019-03-11 23:28:23 +0000

    Flesh out methods

--- setup_data.py
+++ setup_data.py
@@ -1,7 +1,23 @@
-# Create some random data
+import os
 
-# Store the data
+import numpy as np
+
+
+def make_data(ncols=2, size=1000):
+    """Create some random data of the given shape."""
+    shape = (size, ncols)
+    return np.random.randn(*shape)
+
+
+def store_data(data):
+    """Store the given data."""
+    location = '.'
+    out_file = os.path.join(location, 'data.csv')
+    np.savetxt(out_file, data,
+               fmt='%.5f', delimiter=',')
+
 
 if __name__ == "__main__":
-    data = make_data()
-    store_data()
+    np.random.seed(17)
+    my_data = make_data()
+    store_data(my_data)
